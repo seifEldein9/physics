8067361747dc6850245c07bef53d16a96386dfdc
seif Eldein <101245844+seifEldein9@users.noreply.github.com>
2023-06-27 13:28:21 +0300

    Add files via upload

---
+++ python/physics/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name = 'Physics',
+    version = '0.1',
+    author= 'seif Eldein',
+    description= 'A programming library specialized in the rules of physics in general, and more precisely the rules of mechanics and electrical physics and their rates, and it is used to shorten complex rates into simple-to-use functions',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "programming languages :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
